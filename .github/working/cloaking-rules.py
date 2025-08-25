0758a50a86c4d01d88c9c75ab55c50662cb1a8ab
Seva <68725282+sevcator@users.noreply.github.com>
2025-08-25 14:28:34 +0700

    Update cloaking-rules.py

--- .github/working/cloaking-rules.py
+++ .github/working/cloaking-rules.py
@@ -3,9 +3,9 @@ from collections import defaultdict, Counter
 import fnmatch
 
 URL = 'https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/refs/heads/master/hosts'
-remove_domains = ['*xbox*', '*instagram*', '*proton*', '*facebook*', '*torrent*', '*twitch*', '*deezer*', '*dzcdn*', '*weather*', '*fitbit*', '*ggpht*', '*github*', '*4pda*']
+remove_domains = ['*xbox*', '*instagram*', '*proton*', '*facebook*', '*torrent*', '*twitch*', '*deezer*', '*dzcdn*', '*weather*', '*fitbit*', '*ggpht*', '*github*', '*tiktok*', '*imgur*', '*4pda*']
 adblock_ips = {'127.0.0.1', '0.0.0.0'}
-no_simplify_domains = ['*microsoft*', '*bing*', '*goog*', '*github*', '*parsec*', '*imgur*', '*oai*', '*tiktok*', '*archive.org*', '*ttvnw*', '*spotify*', '*scdn.co*']
+no_simplify_domains = ['*microsoft*', '*bing*', '*goog*', '*github*', '*parsec*', '*oai*', '*archive.org*', '*ttvnw*', '*spotify*', '*scdn.co*']
 example_file = 'example-cloaking-rules.txt'
 output_file = 'cloaking-rules.txt'
 
@@ -95,4 +95,5 @@ with open(output_file, 'w', encoding='utf-8') as f:
                 f.write(f"{host} {ip}\n")
 
 print(f"Saved to {output_file}")
+
 
